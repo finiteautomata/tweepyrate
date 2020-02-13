9348de42df691bb9578d34af0c445d15d5e78e58
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-02-13 10:59:13 -0300

    Add streaming functionalities.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # tweepyrate
 A Python library to retrieve huge amounts of tweets
+
+
+## Streaming
+
+`tweepyrate` aids you to avoid getting overwhelmed because of streaming queries that generate a lot of content. For instance, if you try to save tweets inside a `tweepy` listener, chances are high that your connection gets killed by Twitter because it processes them slowly.
+
+To avoid that, we create a queue that just "saves" our tweets, while you can provide another class that does the heavylifting. For instance, let's suppose you want to stream tweets about Messi:
+
+```python
+from tweepyrate.streaming import create_queue, stream_query
+
+class TweetWorker:
+
+    def work(self, status, query):
+        # Do the heavylifting here: saving to database, preprocess, etc
+        print(status["text"])
+
+# Create tweepy app
+app = create_app(...)
+# Create queue and workers
+queue = create_queue(num_workers, TweetWorker)
+
+stream_query("Messi", app, queue)
+```
---
+++ tweepyrate/streaming/__init__.py
@@ -0,0 +1,86 @@
+import tweepy
+import threading
+from queue import Queue
+from .tweet_listener import TweetListener
+
+def create_worker(queue, worker_class):
+    """
+    Creates a non-stop worker to gather tweets
+
+    Arguments:
+    ----------
+
+    queue: queue.Queue
+        A queue to gather tasks from
+
+    worker_class: class or function returning an object
+        The returned object must respond to `work(status, query)`
+
+    Returns:
+    --------
+
+    worker: function
+
+        A function to be used to start a working thread
+    """
+    tweet_worker = worker_class()
+
+    def worker():
+        while True:
+            args = queue.get(block=True)
+            tweet_worker.work(*args)
+            queue.task_done()
+
+    return worker
+
+def create_queue(num_workers, worker_class):
+    """
+    Creates a queue of workers to process incoming tweets for streaming
+
+    Arguments:
+    ----------
+
+    num_workers: int
+        Number of workers to create
+
+    worker_class: class or function returning an object
+        The returned object must respond to `work(status, query)`
+
+    Returns:
+    --------
+
+    a_queue: queue.Queue
+        Queue to push new statuses to
+    """
+    queue = Queue()
+    threads = []
+    for i in range(num_workers):
+        t = threading.Thread(target=create_worker(queue, worker_class))
+        t.start()
+        threads.append(t)
+
+    return queue
+
+def stream_query(query, app, queue, **kwargs):
+    """
+
+    Start streaming query using app and pushing to some queue
+
+    Arguments:
+    ----------
+
+    query: a string
+        The words we want to stream
+
+    app: tweepy.app
+
+    queue: queue.Queue
+        The queue to push new statuses to
+
+    Other argumentes are passed to the `filter` function of tweepy.Stream
+
+    """
+    myStreamListener = TweetListener(query, queue)
+    myStream = tweepy.Stream(auth = app.auth, listener=myStreamListener)
+    myStream.filter(track=[query], is_async=True, **kwargs)
+    return myStreamListener
