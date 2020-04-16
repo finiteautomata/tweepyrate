f7cc80d86e35987d0a613842ab3f97f68b709e35
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-04-16 15:00:22 -0300

    Add some progress messages.

--- tweepyrate/streaming/tweet_listener.py
+++ tweepyrate/streaming/tweet_listener.py
@@ -5,7 +5,7 @@ class TweetListener(tweepy.StreamListener):
     Listener of Twitter Streaming API
 
     This class does not do any real time processing. It just pushes the
-    received status to the respective queue, so another processes do the
+    received status to the respective queue, so another processes do the heavy lifting
 
     We do this to avoid being overwhelmed by the stream. See this link for further information:
 
@@ -14,12 +14,16 @@ class TweetListener(tweepy.StreamListener):
     def __init__(self, query, queue):
         self.query = query
         self.queue = queue
+        self.count = 0
         super().__init__()
 
     def on_status(self, status):
         self.queue.put((status, self.query))
+        self.count += 1
+        if self.count % 100 == 0:
+            print(f"{self.count / 1000:.2f} tweets for {self.query}")
 
     def on_error(self, status_code):
         print(f"Error tipo: {status_code} para query {self.query}")
-        print("Tenemos que esperar...")
+        print("We have to wait...")
         return True
